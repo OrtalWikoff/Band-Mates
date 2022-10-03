e00dedd1ea73397a677b1c13959b4d0b3943f24d
Ortal Wikoff <owikoff@gmail.com>
2022-10-03 09:29:00 -0400

    updated files

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const profileSchema = new Schema({
-  name: { 
+  name: {
     type: String,
     required: true,
     unique: true,
